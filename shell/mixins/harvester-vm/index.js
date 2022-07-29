f1f1e195ba8a7fd32bd7bfbe54adbfac77a890d8
wujun <897415845@qq.com>
2022-07-29 14:53:12 +0800

    HARVESTER: fix template OSType not automatically filled

--- shell/mixins/harvester-vm/index.js
+++ shell/mixins/harvester-vm/index.js
@@ -1263,7 +1263,7 @@ export default {
 
           const oldImageId = old[0]?.image;
 
-          if (this.isCreate && oldImageId === imageId) {
+          if (this.isCreate && oldImageId === imageId && imageId) {
             this.osType = osType;
           }
         }
