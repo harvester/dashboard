98c4712c8bfb3bcc3e97801b335ea4128b24ac13
wujun <897415845@qq.com>
2023-05-15 16:57:10 +0800

    HARVESTER: Restrict volume to integers only

--- pkg/harvester/edit/harvesterhci.io.volume.vue
+++ pkg/harvester/edit/harvesterhci.io.volume.vue
@@ -257,6 +257,7 @@ export default {
 
         <UnitInput
           v-model="storage"
+          v-int-number
           :label="t('harvester.volume.size')"
           :input-exponent="3"
           :output-modifier="true"
